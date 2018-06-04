691ae76eaf80919cb68f79509796a1d55d5c6f75
Mathieu LAURET <mathieu.lauret30@gmail.com>
2018-06-04 23:14:42 +0200

    Update turtle.js
    code quality

--- client/turtle.js
+++ client/turtle.js
@@ -5,25 +5,6 @@ app.renderer.backgroundColor = 0x4556FF;
 
 let turtle;
 
-PIXI.loader
-    .add("box-turtle.png")
-    .load(setup);
-
-
-function setup() {
-    turtle = new PIXI.Sprite(
-        PIXI.loader.resources["box-turtle.png"].texture
-    );
-    turtle.anchor.set(0.5,0.5);
-    turtle.position.set(256,256);
-    turtle.speed = 0;
-    turtle.steer = 0;
-    app.stage.addChild(turtle);
-    app.ticker.add((delta) => gameLoop(delta));
-}
-
-
-
 $(function () {
     var socket = io();
     socket.on("cmd_vel_1", function(msg){
@@ -40,6 +21,23 @@ function gameLoop(delta){
     turtle.y += -turtle.speed * Math.cos(turtle.rotation);
 
 }
+
+function setup() {
+    turtle = new PIXI.Sprite(
+        PIXI.loader.resources["box-turtle.png"].texture
+    );
+    turtle.anchor.set(0.5,0.5);
+    turtle.position.set(256,256);
+    turtle.speed = 0;
+    turtle.steer = 0;
+    app.stage.addChild(turtle);
+    app.ticker.add((delta) => gameLoop(delta));
+}
+
+PIXI.loader
+    .add("box-turtle.png")
+    .load(setup);
+
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
